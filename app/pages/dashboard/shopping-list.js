b6c6fbc5037a0b81f7c7821ebcdad8acc1f76762
Bruno Talanski <brn.talanski@gmail.com>
2023-02-02 15:05:47 -0400

    Update list display

--- app/pages/dashboard/shopping-list.js
+++ app/pages/dashboard/shopping-list.js
@@ -11,14 +11,23 @@ import LayoutUser from '@/components/LayoutUser';
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
 import AddAction from '@/components/ShoppingList/AddAction';
-
+import IconButton from '@mui/material/IconButton';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
+import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
-import { fetchShoppingList, subscribeToShoppingList, unsubscribeToShoppingList } from '@/utils/api';
+
+function ItemHeader(props) {
+  return (
+    <>
+      <Typography component="span" sx={{ fontSize: 18, fontWeight: 'bold'}}>{props.name}</Typography>
+      {props.quantity && <Typography component="span" sx={{ fontSize: 15 }}> (qty {props.quantity})</Typography>}
+    </>
+  )
+}
 
 export default function Dashboard() {
   const router = useRouter();
@@ -98,14 +107,24 @@ export default function Dashboard() {
         <Typography variant="h4" component="h1" gutterBottom>
           Shopping list
         </Typography>
-        <List sx={{ width: '100%', maxWidth: 460, bgcolor: 'background.paper' }}>
+        <List sx={{ width: '100%', maxWidth: 580, bgcolor: 'background.paper' }}>
           {shoppingList.data?.map((record) => {
             const labelId = `checkbox-list-label-${record.id}`;
-            const primary = `${record.item} ${record.quantity ? '(' + record.quantity + ')' : '' }`;
+            const primary = <ItemHeader name={record.item} quantity={record.quantity}></ItemHeader>;
             const secondary = `${record?.expand?.added_by?.name ? 'By ' + record?.expand?.added_by?.name + ' - ' : ''} ${formatDistanceToNow(new Date(record.created))} ago`;
+
             return (
-              <ListItem key={record.id}>
-                <ListItemButton role={undefined} onClick={handleToggle(record.id)} dense>
+              <ListItem 
+                key={record.id}
+                secondaryAction={
+                  <IconButton edge="end" aria-label="edit">
+                    <ModeEditOutlineIcon />
+                  </IconButton>
+                }
+                disablePadding
+                divider
+              >
+                <ListItemButton role={undefined} onClick={handleToggle(record.id)}>
                   <ListItemIcon>
                     <Checkbox
                       edge="start"
@@ -115,7 +134,7 @@ export default function Dashboard() {
                       inputProps={{ 'aria-labelledby': labelId }}
                     />
                   </ListItemIcon>
-                  <ListItemText id={labelId} primary={<Typography sx={{ fontWeight: 'bold'}}>{primary}</Typography>} secondary={secondary}/>
+                  <ListItemText id={labelId} primary={primary} secondary={secondary}/>
                 </ListItemButton>
               </ListItem>
             );
